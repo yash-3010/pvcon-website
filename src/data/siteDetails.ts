4303a10a8219b974876740f4330a005f9eaa3b8d
Yash <yash.barman@kreeti.com>
2026-03-02 12:26:05 +0530

    test version 2

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,6 +1,7 @@
 export const siteDetails = {
     siteName: 'PVCON',
-    siteUrl: 'https://www.pvcon.in',
+    // siteUrl: 'https://www.pvcon.in',
+    siteUrl: 'https://pvcon-website.vercel.app',
     metadata: {
         title: 'PVCON | Pharmacovigilance Consulting & Regulatory Audit Services',
         description: 'PVCON is a specialized pharmacovigilance consulting and audit firm providing QPPV services, PSMF management, regulatory compliance, signal detection, and global drug safety solutions.',
